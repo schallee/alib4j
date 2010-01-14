0ec760e3fbabbac7a0d5fb37b4cb659fd113c73d
schallee <schallee>
2010-01-14 22:38:38 +0000

    Add method to allow ungenericed map to genericed map fudge

--- alib/src/main/java/net/darkmist/alib/generics/GenericFudge.java
+++ alib/src/main/java/net/darkmist/alib/generics/GenericFudge.java
@@ -1,10 +1,31 @@
 package net.darkmist.alib.generics;
 
+import java.util.Map;
+
+/**
+ * Static methods to get around issues with generics... 
+ */
+@SuppressWarnings("unchecked")
 public class GenericFudge
 {
-	@SuppressWarnings("unchecked")
+	/** Only static methods. */
+	private GenericFudge()
+	{
+	}
+
+	/**
+	 * Fudge the return from getClass to include the generic.
+	 */
 	static public <T> Class<? extends T> getClass(T obj)
 	{
 		return (Class<? extends T>)obj.getClass();
+	}
+
+	/**
+	 * Fudge a ungenericified map to be one.
+	 */
+	static public <K,V> Map<K,V> map(Map map)
+	{
+		return map;
 	}
 }
