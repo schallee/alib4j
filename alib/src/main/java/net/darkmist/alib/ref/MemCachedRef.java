9fcf32cee7f573af8c26d1936a5d8a53328b075d
schallee <schallee>
2010-01-14 22:37:54 +0000

    synchronize get instead of using volitile as it's checked multiple times.

--- alib/src/main/java/net/darkmist/alib/ref/MemCachedRef.java
+++ alib/src/main/java/net/darkmist/alib/ref/MemCachedRef.java
@@ -4,7 +4,7 @@ import java.lang.ref.WeakReference;
 
 public abstract class MemCachedRef<T> extends AbstractRef<T>
 {
-	private volatile WeakReference<T> valRef = null;
+	private WeakReference<T> valRef = null;
 
 	/**
 	 * (Re)Create the object.
@@ -14,20 +14,17 @@ public abstract class MemCachedRef<T> extends AbstractRef<T>
 	/**
 	 * Get the cached object. If the object is not in memory, it is recreated.
 	 */
-	// This is not the classic race condition as valRef is volatile.
-	public T get()
+	public synchronized T get()
 	{
-		WeakReference<T> wref;
 		T val;
 
-		if((wref=valRef)!=null && (val = wref.get())!=null)
-			return val;
 		synchronized(this)
 		{
-			if((wref=valRef)!=null && (val = wref.get())!=null)
-				return val;
-			val = make();
-			valRef = new WeakReference<T>(val);
+			if(valRef==null || (val = valRef.get())==null)
+			{
+				val = make();
+				valRef = new WeakReference<T>(val);
+			}
 		}
 		return val;
 	}
