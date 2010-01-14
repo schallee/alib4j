3dda1a701816e20dd226c9e1c23dd41fd04000ce
schallee <schallee>
2010-01-14 22:40:33 +0000

    java doc

--- alib/src/main/java/net/darkmist/alib/exception/ExceptionHandler.java
+++ alib/src/main/java/net/darkmist/alib/exception/ExceptionHandler.java
@@ -1,6 +1,13 @@
 package net.darkmist.alib.exception;
 
+/**
+ * Interface to handle {@link Exception}s.
+ */
 public interface ExceptionHandler
 {
+	/**
+	 * Handle an {@link Exception}.
+	 * @param t the exception to handle.
+	 */
 	public void handleException(Throwable t);
 }
