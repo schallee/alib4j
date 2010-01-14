582445bc3862778ea0b497b9ad082744a22df8b7
schallee <schallee>
2010-01-14 22:32:11 +0000

    Needed for ZipDirTraverser and useful in other places too.

---
+++ alib/src/main/java/net/darkmist/alib/io/CloseIgnoringInputStream.java
@@ -0,0 +1,30 @@
+package net.darkmist.alib.io;
+
+import java.io.InputStream;
+
+import org.apache.commons.io.input.ProxyInputStream;
+
+/**
+ * An {@link InputStream} that ignores {@link InputStream#close()}.
+ * This differes from
+ * {@link org.apache.commons.io.input.CloseShieldInputStream} in that
+ * it does not fake the closing of the underlying InputStream.
+ */
+public class CloseIgnoringInputStream extends ProxyInputStream
+{
+	/**
+	 * Wrap InputStream.
+	 * @param in The InputStream to wrap.
+	 */
+	public CloseIgnoringInputStream(InputStream in)
+	{
+		super(in);
+	}
+
+	/**
+	 * Do nothing.
+	 */
+	public void close()
+	{
+	}
+}
