c534f6596d37edea832d98e15ba0e4c2b2890d7b
schallee <schallee>
2010-01-14 22:34:14 +0000

    change from abstract to private constructor, remove commented out code

--- alib/src/main/java/net/darkmist/alib/str/StrUtil.java
+++ alib/src/main/java/net/darkmist/alib/str/StrUtil.java
@@ -1,38 +1,26 @@
 package net.darkmist.alib.str;
 
-// not changed since qcomm
-// changed now, use StringUtils instead....
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public abstract class StrUtil
+public class StrUtil
 {
 	private static final Class<StrUtil> CLASS = StrUtil.class;
         private static final Log logger = LogFactory.getLog(CLASS);
 
+	private StrUtil()
+	{
+	}
+
+	/**
+	 * Join strings with a delimiter.
+	 * @param delimiter The delimiter to use between strings.
+	 * @param strs The Strings to join.
+	 */
 	// not deprecated because this allows vargs
 	public static final String join(CharSequence delimiter, CharSequence... strs)
 	{
 		return StringUtils.join(strs, delimiter.toString());
-		/*
-		StringBuilder out;
-		int len=0;
-		int c;
-
-		if(strs.length <= 0)
-			return "";
-		for(c=0;c<strs.length;c++)
-			len+=strs[c].length() + delimiter.length();
-		out = new StringBuilder(len);
-		out.append(strs[0]);
-		for(c=1;c<strs.length;c++)
-		{
-			out.append(delimiter);
-			out.append(strs[c]);
-		}
-		return out.toString();
-		*/
 	}
 }
