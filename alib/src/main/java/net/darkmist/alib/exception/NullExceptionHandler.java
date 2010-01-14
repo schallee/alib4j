a346d32feaa9d0f27f112d63ff3500dafd6a816c
schallee <schallee>
2010-01-14 22:39:27 +0000

    change to singleton

--- alib/src/main/java/net/darkmist/alib/exception/NullExceptionHandler.java
+++ alib/src/main/java/net/darkmist/alib/exception/NullExceptionHandler.java
@@ -1,18 +1,32 @@
 package net.darkmist.alib.exception;
 
+/**
+ * {@link ExceptionHandler} that does nothing.
+ */
 public class NullExceptionHandler implements ExceptionHandler
 {
 	private static final NullExceptionHandler singleton = new NullExceptionHandler();
 
-	protected NullExceptionHandler()
+	/**
+	 * Private constructor as this is a singleton.
+	 */
+	private NullExceptionHandler()
 	{
 	}
 
+	/**
+	 * Get singleton instance.
+	 * @return singleton instance.
+	 */
 	public static NullExceptionHandler instance()
 	{
 		return singleton;
 	}
 
+	/**
+	 * Handle an {@link Exception} by doing nothing.
+	 * @param t The exception to handle.
+	 */
 	public void handleException(Throwable t)
 	{
 		// drop it on the ground!
