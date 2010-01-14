d90806af0d12a9926253b0026fe92afb585638c9
schallee <schallee>
2010-01-14 22:41:27 +0000

    Javadoc and remove new since qcomm comment

--- alib/src/main/java/net/darkmist/alib/collection/NonRemovingIterator.java
+++ alib/src/main/java/net/darkmist/alib/collection/NonRemovingIterator.java
@@ -1,15 +1,21 @@
 package net.darkmist.alib.collection;
 
-// new since qcomm
-
 import java.util.Iterator;
 
+/**
+ * Abstract {@link Iterator} that does not support
+ * {@link Iterator#remove}.
+ */
 public abstract class NonRemovingIterator<T> implements Iterator<T>
 {
 	public abstract boolean hasNext();
 
 	public abstract T next();
 
+	/**
+	 * remove method that always trows an exception.
+	 * @throws UnsupportedOperationException always.
+	 */
 	public final void remove()
 	{
 		throw new UnsupportedOperationException(this.getClass().getName() + " does not support remove()");
