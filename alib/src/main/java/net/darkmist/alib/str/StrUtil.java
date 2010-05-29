5058cf8cd7629633dadd154465bdcbd49c8ab0b8
schallee <schallee>
2010-05-29 05:18:34 +0000

    add simple character split that doesn't use regexes

--- alib/src/main/java/net/darkmist/alib/str/StrUtil.java
+++ alib/src/main/java/net/darkmist/alib/str/StrUtil.java
@@ -1,5 +1,8 @@
 package net.darkmist.alib.str;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -8,6 +11,7 @@ public class StrUtil
 {
 	private static final Class<StrUtil> CLASS = StrUtil.class;
         private static final Log logger = LogFactory.getLog(CLASS);
+	private static final String[] EMPTY_STRING_ARRAY = new String[0];
 
 	private StrUtil()
 	{
@@ -22,5 +26,28 @@ public class StrUtil
 	public static final String join(CharSequence delimiter, CharSequence... strs)
 	{
 		return StringUtils.join(strs, delimiter.toString());
+	}
+
+	/**
+	 * Split a string on a delimiter.
+	 * @param str The string to split.
+	 * @param delim The delimiter to split on.
+	 * @return The substrings of str that were seperated by delim.
+	 */
+	public static String[] split(String str, char delim)
+	{
+		List strs;
+		int len;
+		int start,end;
+		
+		if(str == null)
+			return EMPTY_STRING_ARRAY;
+		if((len = str.length())==0)
+			return new String[]{""};
+		strs = new ArrayList(len);
+		for(start=0;start<len && (end = str.indexOf(delim, start))>=0;start=end+1)
+			strs.add(str.substring(start,end));
+		strs.add(str.substring(start));
+		return (String[])strs.toArray(EMPTY_STRING_ARRAY);
 	}
 }
