f3ad10f4214d1c39065a0b3a4cb0f5aa6fe56c60
Ed Schaller <schallee@darkmist.net>
2012-03-08 21:29:01 -0700

    Add empty iterator...

--- core/src/main/java/net/darkmist/alib/collection/IteratorUtil.java
+++ core/src/main/java/net/darkmist/alib/collection/IteratorUtil.java
@@ -3,6 +3,7 @@ package net.darkmist.alib.collection;
 import java.util.Set;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.NoSuchElementException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -46,5 +47,46 @@ public abstract class IteratorUtil
 	public static <T> T[] toSetArray(Iterator<T> i, int sizeGuess)
 	{
 		return (T[])toSet(i, sizeGuess).toArray();
+	}
+
+	private static class EmptyIterator<T> extends NonRemovingIterator<T>
+	{
+		private static EmptyIterator SINGLETON = new EmptyIterator();
+
+		private EmptyIterator()
+		{
+		}
+
+		static <T> EmptyIterator<T> instance()
+		{
+			return SINGLETON;
+		}
+
+		@Override
+		public boolean hasNext()
+		{
+			return false;
+		}
+
+		@Override
+		public T next()
+		{
+			throw new NoSuchElementException("This iterator has no content!");
+		}
+
+		@Override
+		public boolean equals(Object o)
+		{
+			if(o == null)
+				return false;
+			if(!(o instanceof Iterator))
+				return false;
+			return !((Iterator)o).hasNext();
+		}
+	}
+
+	public static <T> Iterator<T> getEmptyIterator()
+	{
+		return EmptyIterator.instance();
 	}
 }
