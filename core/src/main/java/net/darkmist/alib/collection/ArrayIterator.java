0c95190d2a29f0fb6a6657ba37256aa4966188da
Ed Schaller <schallee@darkmist.net>
2012-04-07 11:55:17 -0600

    Some cleanup and make it a private class.
    IteratorUtil.getArrayIterator should be used instead...

--- core/src/main/java/net/darkmist/alib/collection/ArrayIterator.java
+++ core/src/main/java/net/darkmist/alib/collection/ArrayIterator.java
@@ -3,9 +3,11 @@ package net.darkmist.alib.collection;
 import java.util.Iterator;
 
 /**
- * Iterator that iterates over an array.
+ * Iterator that iterates over an array. Note that this does NOT make
+ * a copy of the array so changes to the passed array may cause issues
+ * with iteration.
  */
-public class ArrayIterator<T> extends NonRemovingIterator<T>
+final class ArrayIterator<T> extends NonRemovingIterator<T>
 {
 	private T[] array;
 	private int i;
@@ -13,18 +15,23 @@ public class ArrayIterator<T> extends NonRemovingIterator<T>
 	/**
 	 * Given an array, create an iterator.
 	 * @param array The array to iterate over.
+	 * @throws NullPointerException if array is null.
 	 */
 	public ArrayIterator(T[] array)
 	{
+		if(array == null)
+			throw new NullPointerException("Array was null");
 		this.array = array;
 		this.i = 0;
 	}
 
+	@Override
 	public boolean hasNext()
 	{
 		return (i<array.length);
 	}
 
+	@Override
 	public T next()
 	{
 		return array[i++];
